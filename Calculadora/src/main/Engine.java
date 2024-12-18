90a280aa7fee7dbc45d3ce6f404b0f147926f887
Iván Sierra <ivan.sierra.perez@colegiolitterator.com>
2024-12-18 01:57:24 +0100

    mejor distribucion de los botones

--- Calculadora/src/main/Engine.java
+++ Calculadora/src/main/Engine.java
@@ -1,250 +1,76 @@
 package main;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
- // Marco de la ventana
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 
+public class Engine extends JFrame implements ActionListener {
+    private JPanel contentPanel, displayPanel, buttonPanel;
+    private JTextField display;
+    private JButton n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, divide, multiply, subtract, add, equal, reset;
+    private int num1, num2, result;
+    private char operation; 
 
-public class Engine extends JFrame {
-	
-	 private JFrame frame ;
-	 // Panel general que ocupa toda la ventana
-	 private JPanel contentPanel ;
-	 private Container panelPrincipal;
-	 // Panel norte que contiene el display
-	 private JPanel displayPanel ;
-	 // Panel sur que contiene los botones
-	 private JPanel buttonPanel ;
-	 // Display
-	 private JTextField display ;
-	 // Botones
-	 private JButton n0 ;
-	 private JButton n1 ;
-	 private JButton n2 ;
-	 private JButton n3 ;
-	 private JButton n4 ;
-	 private JButton n5 ;
-	 private JButton n6 ;
-	 private JButton n7 ;
-	 private JButton n8 ;
-	 private JButton n9 ;
-	 private JButton divide ;
-	 private JButton multiply ;
-	 private JButton subtract ;
-	 private JButton add ;
-	 private JButton equal ;
-	 private JButton reset ;
-	 // Tipos de boton
-	 private enum ButtonType { REGULAR , OPERATOR }
-	 // Almacenar temporalmente ciertos valores
-	 private int num1 , num2 , result ;
-	 private char operation ;
-	
-	
-	public Engine() {
-		
-		setSettings();
-	}
-	
-	public void setSettings() {
-		
-		this.panelPrincipal = this.getContentPane();
-		this.setLocation(50, 100);
-		this.setSize(350,350);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		this.contentPanel = new JPanel();
-		this.contentPanel.setLayout(new BorderLayout());
-		this.displayPanel = new JPanel();
-		this.displayPanel.setLayout(new FlowLayout());
-		this.buttonPanel = new JPanel();
-		this.buttonPanel.setLayout(new GridLayout(4, 4, 5, 5));
-		this.buttonPanel.setPreferredSize(new Dimension(200, 200));
-		
-		
-		
-		
-		this.display = new JTextField(20);
-		this.displayPanel.add(display);
-		
-		
-		
-			n0 = new JButton("0");
-			n1 = new JButton("1");
-			n2 = new JButton("2");
-			n3 = new JButton("3");
-			n4 = new JButton("4");
-			n5 = new JButton("5");
-			n6 = new JButton("6");
-			n7 = new JButton("7");
-			n8 = new JButton("8");
-			n9 = new JButton("9");
-			divide = new JButton("/");
-			multiply = new JButton("x");
-			subtract = new JButton("-");
-			add = new JButton("+");
-		equal = new JButton("=");
-		reset = new JButton("C");
-		
-		this.buttonPanel.add(n7);
-		this.buttonPanel.add(n8);
-		this.buttonPanel.add(n9);
-		this.buttonPanel.add(divide);
-		this.buttonPanel.add(n4);
-		this.buttonPanel.add(n5);
-		this.buttonPanel.add(n6);
-		this.buttonPanel.add(multiply);
-		this.buttonPanel.add(n1);
-		this.buttonPanel.add(n2);
-		this.buttonPanel.add(n3);
-		this.buttonPanel.add(subtract);
-		this.buttonPanel.add(reset);
-		this.buttonPanel.add(n0);
-		this.buttonPanel.add(equal);
-		this.buttonPanel.add(add);
-		
-		
-		n0.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "0");
-		    }
-		});
 
-		n1.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "1");
-		    }
-		});
+    public Engine() {
+        setSettings();
+    }
 
-		n2.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "2");
-		    }
-		});
+    public void setSettings() {
+        this.setSize(350, 350);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        contentPanel = new JPanel(new BorderLayout());
+        displayPanel = new JPanel(new FlowLayout());
+        buttonPanel = new JPanel(new GridLayout(4, 4, 5, 5));
+        display = new JTextField(20);
+        display.setFont(new Font("Arial", Font.PLAIN, 18));
+        displayPanel.add(display);
 
-		n3.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "3");
-		    }
-		});
+        n0 = createButton("0");
+        n1 = createButton("1");
+        n2 = createButton("2");
+        n3 = createButton("3");
+        n4 = createButton("4");
+        n5 = createButton("5");
+        n6 = createButton("6");
+        n7 = createButton("7");
+        n8 = createButton("8");
+        n9 = createButton("9");
+        divide = createButton("/");
+        multiply = createButton("x");
+        subtract = createButton("-");
+        add = createButton("+");
+        equal = createButton("=");
+        reset = createButton("C");
 
-		n4.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "4");
-		    }
-		});
+        JButton[] buttons = {n7, n8, n9, divide, n4, n5, n6, multiply, n1, n2, n3, subtract, reset, n0, equal, add};
+        for (JButton button : buttons) {
+            buttonPanel.add(button);
+        }
 
-		n5.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "5");
-		    }
-		});
+        contentPanel.add(displayPanel, BorderLayout.NORTH);
+        contentPanel.add(buttonPanel, BorderLayout.CENTER);
+        this.add(contentPanel);
+        
+    }
 
-		n6.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "6");
-		    }
-		});
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 
-		n7.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "7");
-		    }
-		});
+    public void operation() {
+       
 
-		n8.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "8");
-		    }
-		});
+    }
 
-		n9.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "9");
-		    }
-		});
+    private JButton createButton(String text) {
+        JButton button = new JButton(text);
+        button.addActionListener(this);
+        return button;
+    }
 
-		divide.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "/");
-		    }
-		});
-
-		multiply.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "x");
-		    }
-		});
-
-		subtract.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "-");
-		    }
-		});
-
-		add.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        display.setText(display.getText() + "+");
-		    }
-		});
-		
-		
-		
-		this.contentPanel.add(this.displayPanel, BorderLayout.NORTH);
-		this.contentPanel.add(this.buttonPanel, BorderLayout.SOUTH);
-		
-		this.panelPrincipal.add(this.contentPanel, BorderLayout.CENTER);
-		
-		
-		//this.panelPrincipal.add(contentPanel);
-		
-		
-		
-	}
-	
-	public void setFeaturesButton(JButton _button, ButtonType _type) {
-		
-	}
-
-	
-	public void addActionEvent() {
-		
-	}
-	
-	public void operation() {
-		result = num1 + num2;
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		Object source = e . getSource () ;
-		String input_text = e . getActionCommand () ;
-	}
 }
