fd4775f49b2d702cf8991587c6f435d5fc263d72
Iván Sierra <ivan.sierra.perez@colegiolitterator.com>
2024-12-19 12:56:54 +0100

    AddActionEvent para cada boton

--- Calculadora/src/main/Engine.java
+++ Calculadora/src/main/Engine.java
@@ -7,97 +7,117 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Engine extends JFrame implements ActionListener {
-    private JPanel contentPanel, displayPanel, buttonPanel;
-    private JTextField display;
-    private JButton n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, divide, multiply, subtract, add, equal, reset;
-    private int num1, num2, result;
-    private char operation;
-    public Engine() {
-        setSettings();
-    }
+	private JPanel contentPanel, displayPanel, buttonPanel;
+	private JTextField display;
+	private JButton n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, divide, multiply, subtract, add, equal, reset;
+	private enum ButtonType { REGULAR , OPERATOR }
+	private int num1, num2, result;
+	private char operation;
 
-    public void setSettings() {
-        this.setSize(350, 350);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
+	public Engine() {
+		setSettings();
+	}
 
-        contentPanel = new JPanel(new BorderLayout());
-        displayPanel = new JPanel(new FlowLayout());
-        buttonPanel = new JPanel(new GridLayout(4, 4, 5, 5));
-        display = new JTextField(20);
-        display.setEditable(false);
-        display.setFont(new Font("Arial", Font.PLAIN, 18));
-        displayPanel.add(display);
+	public void setSettings() {
+		this.setSize(350, 350);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
 
-        n0 = createButton("0");
-        n1 = createButton("1");
-        n2 = createButton("2");
-        n3 = createButton("3");
-        n4 = createButton("4");
-        n5 = createButton("5");
-        n6 = createButton("6");
-        n7 = createButton("7");
-        n8 = createButton("8");
-        n9 = createButton("9");
-        divide = createButton("/");
-        multiply = createButton("x");
-        subtract = createButton("-");
-        add = createButton("+");
-        equal = createButton("=");
-        reset = createButton("C");
+		contentPanel = new JPanel(new BorderLayout());
+		displayPanel = new JPanel(new FlowLayout());
+		buttonPanel = new JPanel(new GridLayout(4, 4, 5, 5));
+		display = new JTextField(20);
+		display.setEditable(false);
+		display.setFont(new Font("Arial", Font.PLAIN, 18));
+		displayPanel.add(display);
 
-        JButton[] buttons = {n7, n8, n9, divide, n4, n5, n6, multiply, n1, n2, n3, subtract, reset, n0, equal, add};
-        for (JButton button : buttons) {
-            buttonPanel.add(button);
-        }
+		n0 = createButton("0");
+		n1 = createButton("1");
+		n2 = createButton("2");
+		n3 = createButton("3");
+		n4 = createButton("4");
+		n5 = createButton("5");
+		n6 = createButton("6");
+		n7 = createButton("7");
+		n8 = createButton("8");
+		n9 = createButton("9");
+		divide = createButton("/");
+		multiply = createButton("x");
+		subtract = createButton("-");
+		add = createButton("+");
+		equal = createButton("=");
+		reset = createButton("C");
 
-        contentPanel.add(displayPanel, BorderLayout.NORTH);
-        contentPanel.add(buttonPanel, BorderLayout.CENTER);
-        this.add(contentPanel);
-        this.setVisible(true);
-    }
+		JButton[] buttons = { n7, n8, n9, divide, n4, n5, n6, multiply, n1, n2, n3, subtract, reset, n0, equal, add };
+		for (JButton button : buttons) {
+			buttonPanel.add(button);
+		}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        Object source = e.getSource();
-        String input_text = e.getActionCommand();
+		contentPanel.add(displayPanel, BorderLayout.NORTH);
+		contentPanel.add(buttonPanel, BorderLayout.CENTER);
+		this.add(contentPanel);
+		this.setVisible(true);
+	}
 
-        if(source == equal) {
-        	String[] partes = display.getText().split(" ");
-        	
-        	num1 = Integer.parseInt(partes[0]);
-            operation = partes[1].charAt(0);
-            num2 = Integer.parseInt(partes[2]);
-            operation();
-            display.setText(String.valueOf(result) + " ");
-        }
-        else
-        	
-        	display.setText(display.getText() + input_text + " ");
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		String input_text = e.getActionCommand();
 
-    public void operation() {
-    	switch(operation) {
-    	case '+':
-            result = num1 + num2;
-            break;
-    	case '-':
-    		result = num1 - num2;
-    		break;
-    	case 'x':
-            result = num1 * num2;
-            break;
-    	case '/':
-    		result = num1 / num2;
-    		break;
-    	}
-       
-    }
+		if(source == reset) {
+			display.setText("");
+			
+		}
+		
+		else if (source == equal) {
+			
+			String[] partes = display.getText().split(" ");
 
-    private JButton createButton(String text) {
-        JButton button = new JButton(text);
-        button.addActionListener(this);
-        return button;
-    }
+			num1 = Integer.parseInt(partes[0]);
+			operation = partes[1].charAt(0);
+			num2 = Integer.parseInt(partes[2]);
+			operation();
+			display.setText(String.valueOf(result));
+		} else if (source == add || source == divide || source == multiply || source == subtract) {
+			display.setText(display.getText() + " " + input_text + " ");
+
+		} else
+			display.setText(display.getText() + input_text);
+	}
+
+	public void operation() {
+		switch (operation) {
+		case '+':
+			result = num1 + num2;
+			break;
+		case '-':
+			result = num1 - num2;
+			break;
+		case 'x':
+			result = num1 * num2;
+			break;
+		case '/':
+			result = num1 / num2;
+			break;
+		}
+
+	}
+	public void addActionEvent() {
+		JButton[] buttons = { n7, n8, n9, divide, n4, n5, n6, multiply, n1, n2, n3, subtract, reset, n0, equal, add };
+		for (JButton button : buttons) {
+			button.addActionListener(this);
+		}
+		
+	}
+
+	
+	public void setFeaturesButton(JButton _button, ButtonType _type){
+		
+	}
+	private JButton createButton(String text) {
+		JButton button = new JButton(text);
+		button.addActionListener(this);
+		return button;
+	}
 
 }
